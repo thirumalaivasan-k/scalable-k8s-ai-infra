927d9cffd54243c106dc22593ffc2090740819a4
thirusubash <thiruhcl2016@gmail.com>
2025-07-13 11:03:30 +0000

    non interactive fix: for ssh passphares

--- modules/control-plane-primary/main.tf
+++ modules/control-plane-primary/main.tf
@@ -29,8 +29,8 @@ resource "null_resource" "init_primary_master" {
 
       # Add Kubernetes repo
       "sudo mkdir -p /etc/apt/keyrings",
-      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
-      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
 
       "sudo apt-get update -y",
       "sudo apt-get install -y kubelet kubeadm kubectl",
