b4263d31fcc7516917731c8ba130676ef318ddc0
thirusubash <thiruhcl2016@gmail.com>
2025-07-13 10:35:03 +0000

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@
 locals {
   control_plane_endpoint = "${var.primary_master.ip_address}:6443"
 }
-
+# Primary Control Plane (first master)
 module "control_plane_primary" {
   source                 = "./modules/control-plane-primary"
   hostname               = var.primary_master.hostname
@@ -23,22 +23,23 @@ module "control_plane_primary" {
   network_plugin         = var.network_plugin
 }
 
-
-
+# Secondary Control Plane Nodes (master2, master3)
 module "control_plane_secondary" {
-  source = "./modules/control-plane-secondary"
-
-  ip_address               = each.value.ip_address
-  ssh_user                 = var.ssh_user
-  ssh_private_key          = var.ssh_private_key
+  for_each               = var.master_nodes
+  source                 = "./modules/control-plane-secondary"
+  ip_address             = each.value.ip_address
+  ssh_user               = var.ssh_user
+  ssh_private_key        = var.ssh_private_key
   join_command_script_path = "${path.module}/modules/control-plane-primary/join-command.sh"
 }
 
-
+# Worker Nodes
 module "worker_node" {
-  source = "./modules/worker-node"
-  ip_address               = each.value.ip_address
-  ssh_user                 = var.ssh_user
-  ssh_private_key          = var.ssh_private_key
+  for_each               = var.worker_nodes
+  source                 = "./modules/worker-node"
+  ip_address             = each.value.ip_address
+  ssh_user               = var.ssh_user
+  ssh_private_key        = var.ssh_private_key
   join_command_script_path = "${path.module}/modules/control-plane-primary/join-command.sh"
 }
+
